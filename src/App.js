3f791b93951f714783f719f25df890e729837fb0
Roman Belik <dabonka@yandex.ru>
2018-05-13 23:52:25 +0300

    add bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,80 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { withStyles } from 'material-ui/styles';
+import Drawer from 'material-ui/Drawer';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
 
-class App extends Component {
+const drawerWidth = 320;
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  appFrame: {
+    height: 430,
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+  },
+  'appBar-left': {
+    marginLeft: drawerWidth,
+  },
+  'appBar-right': {
+    marginRight: drawerWidth,
+  },
+  drawerPaper: {
+    position: 'relative',
+    width: drawerWidth,
+  },
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+  },
+});
+
+class App extends React.Component {
+
   render() {
+    const { classes } = this.props;
+
     return (
-      <div>
-        <h1>Chat</h1>
+      <div className={classes.appFrame}>
+        <AppBar
+          position="absolute"
+          className={classes.appBar}
+        >
+          <Toolbar>
+            <Typography variant="title" color="inherit" noWrap>
+              React Chat
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
+        <Drawer
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <div className={classes.toolbar} />
+
+        </Drawer>
+
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <Typography>{'You think water moves fast? You should see ice.'}</Typography>
+        </main>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
