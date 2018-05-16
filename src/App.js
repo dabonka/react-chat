2a391c8f7e6a21d78002c27d3541d97d5a188f5f
Roman Belik <roman.belik@gmail.com>
2018-05-16 17:57:39 +0300

    add changes

--- src/App.js
+++ src/App.js
@@ -1,92 +1,27 @@
 import React from 'react';
 import { withStyles } from 'material-ui/styles';
-import Drawer from 'material-ui/Drawer';
-import AppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
-import Typography from 'material-ui/Typography';
-import SearchBar from 'material-ui-search-bar';
-import ChatList from './ChatList';
+import Sidebar from './components/Sidebar';
+import ChatHeader from './components/ChatHeader';
+import Chat from './components/Chat';
 
-const drawerWidth = 320;
+import { chats, messages } from './mock-data';
 
 const styles = theme => ({
   root: {
-    flexGrow: 1,
-  },
-  appFrame: {
-    height: 430,
-    zIndex: 1,
-    overflow: 'hidden',
     position: 'relative',
     display: 'flex',
     width: '100%',
-  },
-  appBar: {
-    width: `calc(100% - ${drawerWidth}px)`,
-  },
-  'appBar-left': {
-    marginLeft: drawerWidth,
-  },
-  'appBar-right': {
-    marginRight: drawerWidth,
-  },
-  drawerPaper: {
-    position: 'relative',
-    width: drawerWidth,
-  },
-  toolbar: theme.mixins.toolbar,
-  content: {
-    flexGrow: 1,
+    height: '100%',
     backgroundColor: theme.palette.background.default,
-    padding: theme.spacing.unit * 3,
   },
 });
 
-class App extends React.Component {
-
-  render() {
-    const { classes } = this.props;
-
-    return (
-      <div className={classes.appFrame}>
-        <AppBar
-          position="absolute"
-          className={classes.appBar}
-        >
-          <Toolbar>
-            <Typography variant="title" color="inherit" noWrap>
-              React Chat
-            </Typography>
-          </Toolbar>
-        </AppBar>
-
-        <Drawer
-          variant="permanent"
-          classes={{
-            paper: classes.drawerPaper,
-          }}
-        >
-
-          <SearchBar
-            onChange={() => console.log('onChange')}
-            onRequestSearch={() => console.log('onRequestSearch')}
-            style={{
-              margin: '0 auto',
-              maxWidth: 800
-            }}
-          />
-
-          <ChatList />
-
-        </Drawer>
-
-        <main className={classes.content}>
-          <div className={classes.toolbar} />
-          <Typography>{'You think water moves fast? You should see ice.'}</Typography>
-        </main>
-      </div>
-    );
-  }
-}
+const App = ({ classes }) => (
+  <div className={classes.root}>
+    <ChatHeader />
+    <Sidebar chats={chats} />
+    <Chat messages={messages} />
+  </div>
+);
 
 export default withStyles(styles)(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import 'typeface-roboto';
-import registerServiceWorker from './registerServiceWorker';
-import App from './App';
 
 ReactDOM.render(
-  <App />, 
+  <App />,
   document.getElementById('root')
 );
+
 registerServiceWorker();
